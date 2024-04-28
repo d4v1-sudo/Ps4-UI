c83ab93ef2c7b66d8a8bd96164325ef0ca0187e8
d4v1-sudo <davis.dpont@gmail.com>
2024-04-28 20:38:08 -0300

    update

---
+++ README.md
@@ -0,0 +1,55 @@
+# 🎮🕹️👾 - Playstation 4 User Interface - made with HTML, Javascript and pure CSS
+
+![Screenshot](screenshot.png)
+
+## Description
+
+**Ps4-UI** is a project that recreates the user interface (UI) of the PlayStation 4 using HTML, JavaScript, and pure CSS. With it, you can explore a faithful recreation of the iconic PS4 interface right in your web browser.
+
+## Features
+
+- 🖥 **Familiar Interface**: Navigate through an interface closely resembling the PlayStation 4 experience.
+- 🌐 **Simple Navigation**: Explore menus, icons, and PS4 features intuitively.
+- ✏️ **Customization**: Experience the feeling of customization with profile and settings options.
+- 🎮 **Gamepad Support**: Ps4-UI also supports gamepads for an authentic experience.
+
+## How to Use
+
+You can experience Ps4-UI in two ways:
+
+1. **Clone the Repository**:
+
+```shell
+git clone https://github.com/d4v1-sudo/Ps4-UI.git
+```
+
+Then, open the `index.html` file in your browser to start exploring the interface.
+
+2. **Download the ZIP File**:
+
+- Go to the main repository page at [https://github.com/d4v1-sudo/Ps4-UI](https://github.com/d4v1-sudo/Ps4-UI).
+- Click the "Code" button and select "Download ZIP."
+- Extract the ZIP file on your computer and open the `index.html` file in your browser.
+
+## Contributions
+
+Contributions are welcome! Feel free to open issues or send pull requests to enhance this project.
+
+## Thanks to
+
+Arturo Cabrera: [@Pixmy](https://www.github.com/Pixmy)
+
+[Original project here](https://codepen.io/Pixmy/pen/qaYQoV)
+
+## License
+
+This project is licensed under the [MIT License](LICENSE). Feel free to use it for personal or commercial purposes.
+
+Enjoy exploring the PlayStation 4 interface recreated in Ps4-UI! If you have any questions or feedback, don't hesitate to get in touch.
+
+## Note
+
+This repo doesn't have a commercial purpose; it's an HTML, JS and CSS project made to simulate the PS4 UI.
+
+
+<a href="https://visitorbadge.io/status?path=https%3A%2F%2Fgithub.com%2Fd4v1-sudo%2FPs4-UI"><img src="https://api.visitorbadge.io/api/visitors?path=https%3A%2F%2Fgithub.com%2Fd4v1-sudo%2FPs4-UI&label=Thanks%20for%20dropping%20in&labelColor=%23000000&countColor=%23FFFFFF" /></a>
---
+++ assets/js/script.js
@@ -0,0 +1,460 @@
+/*PS4 Interface Version 2.0.0
+  thanks to
+  https://codepen.io/Kapilnemo/pen/bwYoPZ
+  and
+  https://codepen.io/rstacruz/pen/oxJqNv
+  and
+  https://codepen.io/Pixmy/pen/qaYQoV
+*/
+$(document).ready(function(){
+		$(".gameText").hide();
+		$('.gameTitle').hide();
+		$('#store').hide();
+		timeout();
+
+		// Variables for current position
+		var x, y;
+
+		function handleMouse(e) {
+		  // Verify that x and y already have some value
+		  if (x && y) {
+		    // Scroll window by difference between current and previous positions
+		    window.scrollBy(e.clientX - x, e.clientY - y);
+		  }
+
+		  // Store current position
+		  x = e.clientX;
+		  y = e.clientY;
+		}
+
+    //Clock
+function currentTime() {
+  let date = new Date(); 
+  let hh = date.getHours();
+  let mm = date.getMinutes();
+
+   hh = (hh < 10) ? "0" + hh : hh;
+   mm = (mm < 10) ? "0" + mm : mm;
+    
+   let time = hh + ":" + mm;
+
+  document.getElementById("clock").innerText = time; 
+  let t = setTimeout(function(){ currentTime() }, 1000);
+}
+    
+currentTime();
+    
+    //Controller
+const rAF = window.mozRequestAnimationFrame || window.requestAnimationFrame;
+let current = 1;
+let focusable = document.querySelectorAll('a.href, [tabindex], [tabindex]:not([tabindex="-1"])');
+
+window.addEventListener('gamepadconnected', function (e) {
+    updateLoop();
+});
+//Audios
+function play_a() {
+    var audio = new Audio('./assets/js/navigate.mp3');
+    audio.play();
+}
+    
+function play_b() {
+    var audio = new Audio('./assets/js/startup.mp3');
+    audio.play();
+}
+    
+function play_c() {
+    var audio = new Audio('./assets/js/shutdown.mp3');
+    audio.play();
+}
+    
+function play_d() {
+    var audio = new Audio('./assets/js/reward.mp3');
+    audio.play();
+}
+// event listener for vibration button
+const btnVibration = document.querySelector('#vibration');
+if (btnVibration) {
+    btnVibration.addEventListener('click', function (e) {
+        hapticFeedback();
+    });
+}
+
+function hapticFeedback() {
+    navigator.getGamepads()[0].vibrationActuator.playEffect('dual-rumble', {
+        startDelay: 0,
+        duration: 1500,
+        weakMagnitude: 1,
+        strongMagnitude: 1
+    });
+}
+
+function shutdown(){
+    play_c();
+    setTimeout(() => {
+        document.location.reload();
+    }, 1420);
+};
+
+const shutdown2 = document.querySelector('#shutdown');
+if (shutdown2) {
+    shutdown2.addEventListener('click', function (e) {
+        shutdown();
+        navigator.getGamepads()[0].vibrationActuator.playEffect('dual-rumble', {
+        startDelay: 0,
+        duration: 250,
+        weakMagnitude: 1,
+        strongMagnitude: 1
+    });
+    });
+}
+//Navigate on page
+function jump_last(index) {
+    current = index % focusable.length;
+    index++;
+    if (current <= 0 || current < 15) {
+        current = 15;
+    }
+    focusable[current].focus();
+}
+    
+function jump_first() {
+  current = 1;
+  focusable[current].focus();
+}
+    
+function nextItem(index) {
+  index++;
+  current = index % focusable.length;
+  if (current === 0 && index !== 1) {
+    current = 1;
+  }
+  focusable[current].focus();
+}
+
+function prevItem(index) {
+    index--;
+    current = index % focusable.length;
+    if (current < 1) {
+        current = focusable.length - 1;
+    }
+    focusable[current].focus();
+}
+
+let index = 0;
+    
+function upItem() {
+    current = index % focusable.length;
+  if (index === 0) {
+    index = focusable.length - 1;
+  } else {
+    let currentOffsetTop = focusable[index].offsetTop;
+    let prevOffsetTop = focusable[index - 1]?.offsetTop; // add a check for undefined here
+    while (index > 0 && currentOffsetTop === prevOffsetTop) {
+      index--;
+      currentOffsetTop = focusable[index].offsetTop;
+      prevOffsetTop = focusable[index - 1]?.offsetTop; // add a check for undefined here
+    }
+    focusable[index].focus();
+  }
+}
+
+function downItem() {
+  if (index === focusable.length - 1) {
+    index = 0;
+  } else {
+    let currentOffsetTop = focusable[index].offsetTop;
+    let nextOffsetTop = focusable[index + 1]?.offsetTop; // add a check for undefined here
+    while (index < focusable.length - 1 && currentOffsetTop === nextOffsetTop) {
+        index++;
+        currentOffsetTop = focusable[index].offsetTop;
+        nextOffsetTop = focusable[index + 1]?.offsetTop; // add a check for undefined here
+    }
+    focusable[index].focus();
+  }
+}
+
+function clickItem(index) {
+    if (focusable[index]) {
+        focusable[index].click();
+    }
+}
+
+var botaoPsClicado = false;
+var tempoAcabou = false;
+
+function unlock_page() {
+    if (tempoAcabou) {
+        play_b()
+        document.getElementById("restoDaPagina").style.display = "block";
+        document.getElementById("imagem").style.display = "none";
+        botaoPsClicado = true;
+        document.getElementById("mensagem").style.display = "none"; // Oculta a mensagem quando o botão é clicado
+    }
+}
+    
+function updateLoop() {
+    let gp = navigator.getGamepads()[0];
+    console.log(current);
+    console.log(gp);
+
+    let controllerType = "";
+    if (gp.id === "Wireless Controller (STANDARD GAMEPAD Vendor: 054c Product: 09cc)") {
+        controllerType = "Dualshock 4";
+    } else if (gp.id === "Xbox One Wired Controller (STANDARD GAMEPAD Vendor: 045e Product: 02ea)") {
+        controllerType = "Xbox Wireless Controller";
+    }
+    console.log(controllerType);
+
+    switch (true) {
+        case gp.buttons[0].pressed:
+            clickItem(current);
+            break;
+        case gp.buttons[14].pressed:
+        case gp.axes[0] === -1:
+            prevItem(current);
+            play_a();
+            break;
+        case gp.buttons[15].pressed:
+        case gp.axes[0] === 1:
+            nextItem(current);
+            play_a();
+            break;
+        case gp.axes[1] === -1: // Down
+            case gp.buttons[13].pressed:
+            downItem(current);
+            play_a();
+            break;
+        case gp.axes[1] === 1: // Up
+            case gp.buttons[12].pressed:
+            upItem(current);
+            play_a();
+            break;
+        case gp.buttons[16].pressed && tempoAcabou && !botaoPsClicado:
+            unlock_page();
+            break;
+        case gp.buttons[9].pressed:
+        case gp.buttons[8].pressed:
+            hapticFeedback();
+            break;
+        case gp.buttons[5].pressed:
+            jump_last(current);
+            play_a();
+            break;
+        case gp.buttons[4].pressed:
+            jump_first(current);
+            play_a();
+            break;
+        default:
+            break;
+    }
+
+    setTimeout(function () {
+        rAF(updateLoop);
+    }, 130);
+}
+
+//Navigate using keyboard
+
+document.addEventListener("keydown", function (event) {
+    switch (event.code) {
+        case "ArrowUp":
+        case "KeyW":
+            upItem(current);
+            play_a();
+            break;
+        case "ArrowDown":
+        case "KeyS":
+            downItem(current);
+            play_a();
+            break;
+        case "ArrowLeft":
+        case "KeyA":
+            prevItem(current);
+            play_a();
+            break;
+        case "ArrowRight":
+        case "KeyD":
+            nextItem(current);
+            play_a();
+            break;
+        case "Enter":
+        case "Space":
+            clickItem(current);
+            unlock_page();
+            break;
+        case "ShiftLeft":
+            jump_first(current);
+            play_a();
+            break;
+        case "ShiftRight":
+            jump_last(current);
+            play_a();
+            break;
+    }
+});
+
+// Timer to unlock buttons
+setTimeout(function () {
+    timeIsUp = true;
+}, 15000);
+
+let counter = 15;
+let timer = setInterval(function () {
+    console.log(counter + " seconds remaining...");
+    counter--;
+    if (counter < 0) {
+        clearInterval(timer);
+        console.log("Time's up");
+    }
+}, 1000);
+
+// Timer to unlock buttons
+setTimeout(function () {
+    timeIsUp = true;
+    document.getElementById("message").style.display = "block";
+}, 15000);
+
+// Notification
+function showNotification() {
+    const title = "Hello World!";
+    const message = "Achieved the first trophy.";
+    const icon = "./assets/img/icon.png";
+
+    const notification = document.createElement("div");
+    notification.classList.add("notification-container");
+
+    const iconContainer = document.createElement("div");
+    iconContainer.classList.add("icon-container");
+    const iconImg = document.createElement("img");
+    iconImg.setAttribute("src", icon);
+    iconContainer.appendChild(iconImg);
+    notification.appendChild(iconContainer);
+
+    const contentContainer = document.createElement("div");
+    contentContainer.classList.add("content-container");
+    const titleElem = document.createElement("h3");
+    titleElem.classList.add("notification-title");
+    titleElem.textContent = title;
+    const messageElem = document.createElement("p");
+    messageElem.classList.add("notification-message");
+    messageElem.textContent = message;
+    contentContainer.appendChild(titleElem);
+    contentContainer.appendChild(messageElem);
+    notification.appendChild(contentContainer);
+
+    document.body.appendChild(notification);
+    playSound();
+
+    setTimeout(() => {
+        notification.classList.add("slide-in");
+        setTimeout(() => {
+            notification.classList.remove("slide-in");
+            notification.classList.add("slide-out");
+            setTimeout(() => {
+                notification.remove();
+            }, 8000);
+        }, 3000);
+    }, 100);
+}
+
+const plusButton = document.getElementById('plus');
+
+plusButton.addEventListener('click', function() {
+    showNotification();
+});
+
+
+
+		// Assign handleMouse to mouse movement events
+		document.onmousemove = handleMouse;
+
+		 $(window).scroll(function(){
+	        if ($(this).scrollTop() > 10) {
+	            $('header').css("background-color","rgba(34,123,165,.8)");
+	        } else {
+	            $('header').css("background-color","rgba(255,255,255,0)");
+	        }
+	    });
+	});
+
+	var time = 0;
+
+function timeout() {
+    setTimeout(function () {
+        if (time === 0) {
+            $("#textInfo").text("God of War Ragnarok now available");
+            $("#textInfo").addClass("animated slideInDown");
+            time++;
+        } else if (time === 1) {
+            $("#textInfo").text("The Last of Us Part II new update");
+            $("#textInfo").addClass("animated slideInDown");
+            time++;
+        } else if (time === 2) {
+            $("#textInfo").text("Ghosts'n Goblins demo available now");
+            $("#textInfo").addClass("animated slideInDown");
+            time = 0;
+        }
+
+        setTimeout(function () {
+            $("#textInfo").removeClass("animated slideInDown");
+        }, 1000);
+        timeout();
+    }, 4000);
+}
+
+
+
+	$(".squareGame").hover(
+		function(){
+			$(this).find('.gameText').show();
+			$(this).find('.gameTitle').show();
+			$(this).find('#store').show();
+		},
+		function(){
+			$(this).find('.gameText').hide();
+			$(this).find('.gameTitle').hide();
+			$(this).find('#store').hide();
+		}
+	);
+
+// Improvement for focus in games
+document.getElementById("games").addEventListener("focusin", function (event) {
+  const target = event.target;
+
+  if (target.classList.contains("imgGame")) {
+    const gameText = target.nextElementSibling;
+    gameText.style.display = "block";
+    const gameTitle = target.nextElementSibling.nextElementSibling;
+    gameTitle.style.display = "block";
+    target.style.opacity = "1";
+  }
+});
+
+document.getElementById("games").addEventListener("focusout", function (event) {
+  const target = event.target;
+
+  if (target.classList.contains("imgGame")) {
+    const gameText = target.nextElementSibling;
+    gameText.style.display = "none";
+    const gameTitle = target.nextElementSibling.nextElementSibling;
+    gameTitle.style.display = "none";
+    target.style.opacity = "1";
+  }
+});
+
+// Improvement in the display of game images
+$(".squareGame").hover(
+  function () {
+    $(this).find('.gameText').show();
+    $(this).find('.gameTitle').show();
+    $(this).find('#store').show();
+    $(this).find('.imgGame').css("opacity", "1"); // Display the image immediately
+  },
+  function () {
+    $(this).find('.gameText').hide();
+    $(this).find('.gameTitle').hide();
+    $(this).find('#store').hide();
+    $(this).find('.imgGame').css("opacity", "1"); // Set the original image opacity
+  }
+);
